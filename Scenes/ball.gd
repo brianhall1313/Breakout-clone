ca87a1bc3f41e52ff00394cf991eada55a1daac0
Brian <brianhall1313@gmail.com>
2024-02-18 12:23:10 -0800

    bounce fixed

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	if (velocity == Vector2.ZERO):
-		velocity.y = 50
+		velocity.y = 150
 		velocity.x = 15
 	var result = move_and_collide(velocity * delta)
 	if result:
-		var obj = result.get_collider()
-		#velocity.bounce(result.get_normal()) 
-		velocity = result.get_normal() * velocity
+		#var obj = result.get_collider()
+		velocity = velocity.bounce(result.get_normal()) 
+		#velocity = result.get_normal() * velocity
 	
